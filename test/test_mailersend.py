8fccb2326adfea2485825ebe457bdd78c9f50e18
Alex Orfanos <alexandros@mailerlite.com>
2020-12-08 07:45:05 +0200

    test commit

--- test/test_mailersend.py
+++ test/test_mailersend.py
@@ -5,9 +5,15 @@ import unittest
 class BasicTests(unittest.TestCase):
     def test_api_base_not_none(self):
         """
-        Basic version checking
+        Basic checking
         """
         self.assertIsNotNone(mailersend.API_BASE)
+
+    def test_can_instantiate_object(self):
+        self.assertIsNotNone(mailersend.NewApiClient())
+
+    def test_can_make_api_call(self):
+        pass
 
 
 if __name__ == "__main__":
