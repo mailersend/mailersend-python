339b660268d7a08e8afdcac13c3bef31b73cd00f
Alex Orfanos <alexandros@mailerlite.com>
2021-07-15 10:04:32 +0300

    add analytics endpoints

--- mailersend/activity/activity.py
+++ mailersend/activity/activity.py
@@ -12,31 +12,6 @@ class NewActivity(base.NewAPIClient):
             baseObj.mailersend_api_key,
         )
 
-    def getActivityByDate(self, domainId, dateFrom, dateTo, groupBy, event):
-        self.domainId = domainId
-        self.dateFrom = dateFrom
-        self.dateTo = dateTo
-        self.groupBy = groupBy
-        self.event = event
-
-        _data = {
-            "date_from": dateFrom,
-            "date_to": dateTo,
-            "event": event,
-        }
-
-        if domainId is not None:
-            _data["domain_id"] = domainId
-
-        if groupBy is not None:
-            _data["group_by"] = groupBy
-
-        request = requests.get(
-            f"{self.api_base}/analytics/date", headers=self.headers_default, json=_data
-        )
-
-        return request.text
-
     def getDomainActivity(
         self, domainId, page=None, limit=None, dateFrom=None, dateTo=None, event=None
     ):
