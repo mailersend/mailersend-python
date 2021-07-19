2c2c3ec590578b7547bd66501e2c868d0b114008
Alex Orfanos <alexandros@mailerlite.com>
2021-07-19 16:23:02 +0300

    update version

--- mailersend/__init__.py
+++ mailersend/__init__.py
@@ -3,5 +3,5 @@ MailerSend Official Python DSK
 @maintainer: Alexandros Orfanos (alexandros at remotecompany dot com)
 """
 
-__version_info__ = ("0", "1", "2")
+__version_info__ = ("0", "1", "3")
 __version__ = ".".join(__version_info__)
