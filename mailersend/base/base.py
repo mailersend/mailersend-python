a688e6306b34874e5a4d7e7bac150dd2c298e6bc
flanciskinho <10118328+flanciskinho@users.noreply.github.com>
2021-08-04 13:30:14 +0200

    Although the NewAPIClient constructor has parameters defined, these were not used

--- mailersend/base/base.py
+++ mailersend/base/base.py
@@ -25,15 +25,15 @@ class NewAPIClient:
         NewAPIClient constructor
         """
 
-        self.api_base = API_BASE
-        self.mailersend_api_key = API_KEY
-        self.headers_auth = f"Bearer {self.mailersend_api_key}"
+        self.api_base = API_BASE if api_base is None else api_base
+        self.mailersend_api_key = API_KEY if mailersend_api_key is None else mailersend_api_key
+        self.headers_auth = f"Bearer {self.mailersend_api_key}" if headers_auth is None else headers_auth
         self.headers_default = {
             "Content-Type": "application/json",
             "X-Requested-With": "XMLHttpRequest",
             "User-Agent": "MailerSend-Client-python-v1",
             "Authorization": f"{self.headers_auth}",
-        }
+        } if headers_default is None else headers_default
 
 
 def generate_config_change_json_body(key, value):
