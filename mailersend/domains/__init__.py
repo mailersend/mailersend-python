5f48d2fc891ff18368475323676ea9c74d7a93be
k4mrul <kamrul@fish.cat>
2021-11-25 14:16:16 +0600

    add/verify domain, get dns

--- mailersend/domains/__init__.py
+++ mailersend/domains/__init__.py
@@ -47,6 +47,24 @@ class NewDomain(base.NewAPIClient):
         )
         return request.text
 
+    def add_domain(self, key, value):
+        """
+        Add a domain
+
+        @params:
+          key (str): A key option to add
+          value (str): The respective value of key to add
+
+        """
+        data = {f"{key}": value}
+
+        request = requests.post(
+            f"{self.api_base}/domains",
+            headers=self.headers_default,
+            json=data,
+        )
+        return request.text
+
     def delete_domain(self, domain_id):
         """
         Delete a domain
@@ -94,3 +112,36 @@ class NewDomain(base.NewAPIClient):
             json=data,
         )
         return request.text
+
+    def get_dns_records(self, domain_id):
+        """
+        Returns the JSON response of dns records
+
+        @params:
+          domain_id (str): A domain ID
+
+        """
+
+        request = requests.get(
+            f"{self.api_base}/domains/{domain_id}/dns-records",
+            headers=self.headers_default
+        )
+        return request.text
+
+    def verify_domain(self, domain_id):
+        """
+        Returns the JSON response of verified domain
+
+        @params:
+          domain_id (str): A domain ID
+
+        """
+
+        request = requests.get(
+            f"{self.api_base}/domains/{domain_id}/verify",
+            headers=self.headers_default
+        )
+        return request.text
+
+
+
