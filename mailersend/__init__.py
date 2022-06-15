f4e0421a239d2e89ebf43c545dd19247cf9e2b9c
Igor Hrček <igor@hrcek.rs>
2022-06-15 15:52:03 +0200

    feat: Version change

--- mailersend/__init__.py
+++ mailersend/__init__.py
@@ -1,7 +1,7 @@
 """
 MailerSend Official Python DSK
-@maintainer: Igor Hrček (igor at remotecompany dot com)
+@maintainer: Igor Hrček (igor at mailerlite dot com)
 """
 
-__version_info__ = ("0", "3", "1")
+__version_info__ = ("0", "3", "2")
 __version__ = ".".join(__version_info__)
