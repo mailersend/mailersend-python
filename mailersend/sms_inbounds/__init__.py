f295cb2489a8324c92fcfc48e44d990e9d9b1997
Igor Hrček <igor@hrcek.rs>
2022-06-16 13:32:22 +0200

    feat: Added ability to retrieve information about SMS Inbound routes

--- mailersend/sms_inbounds/__init__.py
+++ mailersend/sms_inbounds/__init__.py
@@ -15,10 +15,45 @@ class NewSmsInbounds(base.NewAPIClient):
     pass
 
     def get_inbound_routes(self, sms_number_id=None, enabled=None, page=1, limit=25):
-        pass
+        """
+        Get a list of SMS inbound routes.
+
+        @params:
+          sms_number_id (string)
+          enabled (bool)
+          page (int)
+          limit (int)
+        """
+
+        passed_arguments = locals()
+        query_params = {}
+
+        for key, value in passed_arguments.items():
+            if key != "self":
+                if key == "enabled":
+                    query_params[key] = int(value)
+                else:
+                    query_params[key] = value
+
+        request = requests.get(
+            f"{self.api_base}/sms-inbounds", headers=self.headers_default, params=query_params
+        )
+
+        return f"{request.status_code}\n{request.text}"
 
     def get_inbound_route(self, sms_inbound_id):
-        pass
+        """
+        Get a single SMS inbound route.
+
+        @params:
+          sms_inbound_id (string)
+        """
+
+        request = requests.get(
+            f"{self.api_base}/sms-inbounds/{sms_inbound_id}", headers=self.headers_default
+        )
+
+        return f"{request.status_code}\n{request.text}"
 
     def create_inbound_route(self, sms_number_id, name, forward_url, filter, enabled=True):
         pass
