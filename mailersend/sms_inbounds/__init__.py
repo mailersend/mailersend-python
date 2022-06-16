09ed67033406f904793553c63383e13a90fe88c4
Igor Hrček <igor@hrcek.rs>
2022-06-16 13:41:04 +0200

    feat: Added ability to create and update SMS Inbound routes

--- mailersend/sms_inbounds/__init__.py
+++ mailersend/sms_inbounds/__init__.py
@@ -55,11 +55,59 @@ class NewSmsInbounds(base.NewAPIClient):
 
         return f"{request.status_code}\n{request.text}"
 
-    def create_inbound_route(self, sms_number_id, name, forward_url, filter, enabled=True):
-        pass
+    def create_inbound_route(self, sms_number_id, name, forward_url, filter={}, enabled=True):
+        """
+        Add an SMS inbound route.
 
-    def update_inbound_route(self, sms_inbound_id, sms_number_id, name, forward_url, filter, enabled=True):
-        pass
+        @params:
+          sms_number_id (string)
+          name (string)
+          forward_url (string)
+          filter (object)
+          enabled (bool)
+        """
+
+        data = {
+            "sms_number_id": sms_number_id,
+            "name": name,
+            "forward_url": forward_url,
+            "filter": filter,
+            "enabled": int(enabled)
+        }
+
+        request = requests.post(
+            f"{self.api_base}/sms-inbounds", headers=self.headers_default, json=data
+        )
+
+        return f"{request.status_code}\n{request.text}"
+
+    def update_inbound_route(self, sms_inbound_id, name=None, forward_url=None, filter=None, enabled=None):
+        """
+        Update an inbound route.
+
+        @params:
+          sms_inbound_id (string)
+          name (string)
+          forward_url (string)
+          filter (object)
+          enabled (bool)
+        """
+
+        passed_arguments = locals()
+        data = {}
+
+        for key, value in passed_arguments.items():
+            if key != "self" and key != "sms_inbound_id" and value is not None:
+                if key == "enabled":
+                    data[key] = int(value)
+                else:
+                    data[key] = value
+
+        request = requests.put(
+            f"{self.api_base}/sms-inbounds/{sms_inbound_id}", headers=self.headers_default, json=data
+        )
+
+        return f"{request.status_code}\n{request.text}"
 
     def delete_inbound_route(self, sms_inbound_id):
         """
