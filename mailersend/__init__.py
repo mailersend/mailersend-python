bd273f99d2f550fabfb5e43d01f3aaf3ed7cf22a
Igor Hrček <igor@hrcek.rs>
2022-06-22 17:32:17 +0200

    feat: Added description information for PyPi

--- mailersend/__init__.py
+++ mailersend/__init__.py
@@ -3,5 +3,5 @@ MailerSend Official Python DSK
 @maintainer: Igor Hrček (igor at mailerlite dot com)
 """
 
-__version_info__ = ("0", "3", "6")
+__version_info__ = ("0", "3", "7")
 __version__ = ".".join(__version_info__)
