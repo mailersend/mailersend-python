606433e3ab68f0ef635fd1cd8d3b6b9817e6daa5
Igor Hrček <igor@hrcek.rs>
2022-07-27 14:13:08 +0200

    docs: Updated documentation for SMS Inbouds

--- mailersend/__init__.py
+++ mailersend/__init__.py
@@ -3,5 +3,5 @@ MailerSend Official Python DSK
 @maintainer: Igor Hrček (igor at mailerlite dot com)
 """
 
-__version_info__ = ("0", "3", "4")
+__version_info__ = ("0", "3", "8")
 __version__ = ".".join(__version_info__)
