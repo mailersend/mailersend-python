a8e0bce6384d137890bdf10986326010cbe22700
Igor Hrček <igor@hrcek.rs>
2023-09-12 17:48:35 +0200

    fix: Removed pre-commit as dependency

--- mailersend/__init__.py
+++ mailersend/__init__.py
@@ -3,5 +3,5 @@ MailerSend Official Python DSK
 @maintainer: Igor Hrček (igor at mailerlite dot com)
 """
 
-__version_info__ = ("0", "5", "3")
+__version_info__ = ("0", "5", "5")
 __version__ = ".".join(__version_info__)
