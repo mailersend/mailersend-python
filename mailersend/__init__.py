e3f047bb43e55c14ad17435bc15fb9ef3e0c8e82
Igor Hrček <igor@hrcek.rs>
2025-04-25 14:42:53 +0200

    feat: Added support for list_unsubscribe

--- mailersend/__init__.py
+++ mailersend/__init__.py
@@ -3,5 +3,5 @@ MailerSend Official Python DSK
 @maintainer: Igor Hrček (igor at mailerlite dot com)
 """
 
-__version_info__ = ("0", "5", "8")
+__version_info__ = ("0", "6", "0")
 __version__ = ".".join(__version_info__)
